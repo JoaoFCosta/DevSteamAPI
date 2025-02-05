eb72970636b8862fca50386e17037cbe99a079db
JoaoFCosta <joaofiamenguicosta@gmail.com>
2025-02-05 11:53:45 -0300

    Adicionar arquivos de projeto.

---
+++ DevSteamAPI/Data/DevSteamAPIContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DevSteamAPI.Data
+{
+    public class DevSteamAPIContext : DbContext
+    {
+        public DevSteamAPIContext(DbContextOptions<DevSteamAPIContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ DevSteamAPI/Program.cs
@@ -0,0 +1,90 @@
+using DevSteamAPI.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+//Configurar a conexão com o banco de dados
+builder.Services.AddDbContext<DevSteamAPIContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configurar o CORS para aceitar requisições de qualquer origem
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        });
+});
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+// Adionar o Swagger com JWT Bearer
+builder.Services.AddSwaggerGen(c =>
+{
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+    {
+        new OpenApiSecurityScheme
+        {
+        Reference = new OpenApiReference
+            {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+            },
+            Scheme = "oauth2",
+            Name = "Bearer",
+            In = ParameterLocation.Header,
+
+        },
+        new List<string>()
+        }
+    });
+});
+
+// Serviço de EndPoints do Identity Framework
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 4;
+})
+    .AddEntityFrameworkStores<DevSteamAPIContext>()
+    .AddDefaultTokenProviders(); // Adiocionando o provedor de tokens padrão
+
+var app = builder.Build();
+
+// Swagger em ambiente de produção
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// Mapear os EndPoints padrão do Identity Framework
+app.MapGroup("/Users").MapIdentityApi<IdentityUser>();
+app.MapGroup("/Roles").MapIdentityApi<IdentityRole>();
+
+app.UseHttpsRedirection();
+
+// Permitir a autenciação e autorização de qualquer origem
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseCors("AllowAll");
+
+app.MapControllers();
+
+app.Run();
